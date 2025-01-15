38709382c19bb23204f34ce29d5aaf8e7c73bd3a
radman1007 <radman.cod110@gmail.com>
2025-01-15 22:12:38 +0330

    migrate

--- home/views.py
+++ home/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import *
+from blog.models import Blog
 
 def home(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    project_categories = ProjectCategory.objects.all()
+    projects = Project.objects.all()
+    works = Work.objects.all()
+    skills = Skill.objects.all()
+    blogs = Blog.objects.all()
+    context = {
+        'services' : services,
+        'project_categories' : project_categories,
+        'projects' : projects,
+        'works' : works,
+        'skills' : skills,
+        'blogs' : blogs,
+    }
+    return render(request, 'index.html', context)
