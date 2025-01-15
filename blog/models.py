848c48a9888315aa2a43bd6b4c1334fd054f6669
radman1007 <radman.cod110@gmail.com>
2025-01-15 21:18:50 +0330

    add a projects models and edit blog model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class BlogCategory(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
@@ -13,7 +13,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     author = models.CharField(max_length=100)
-    category = models.ManyToManyField(Category, related_name='category')
+    category = models.ManyToManyField(BlogCategory, related_name='category')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ProjectCategory(models.Model):
+    name = models.CharField(100)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    category = models.ManyToManyField(ProjectCategory, related_name='category')
+    customer = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
