0a6fbadc8806a2e85e44a7f441328a1fc0b78aac
radman1007 <radman.cod110@gmail.com>
2025-01-15 21:25:24 +0330

    migrate

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ProjectCategory(models.Model):
-    name = models.CharField(100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
