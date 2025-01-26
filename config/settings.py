bb348c0c889c95e73dfdb0b4fec66a00ec5792c6
radman1007 <radman.cod110@gmail.com>
2025-01-26 17:51:56 +0330

    import os and create media url and root

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from . import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
