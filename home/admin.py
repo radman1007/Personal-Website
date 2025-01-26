004a74e19c443699cde3538425ae9cc4c18d8601
radman1007 <radman.cod110@gmail.com>
2025-01-26 17:05:51 +0330

    migrate data

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Service, ProjectCategory, Project, Work, Skill
+from .models import *
 
 admin.site.register(Service)
 admin.site.register(ProjectCategory)
 admin.site.register(Project)
 admin.site.register(Work)
+admin.site.register(Education)
 admin.site.register(Skill)
+admin.site.register(Contact)
